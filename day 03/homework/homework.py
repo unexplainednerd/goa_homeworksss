378e7b48a68a52a65ac8f56522f107d55b13dcc4
unexplainednerd <157606755+unexplainednerd@users.noreply.github.com>
2024-03-22 19:24:53 +0400

    edited folders

--- day 03/homework/homework.py
+++ day 03/homework/homework.py
@@ -1,1 +1,11 @@
-print("გავიცანი და დავუმეგობრდი: vano motiashvils")
+print("my family members: ")
+
+print("my mom : xatia")
+
+print("my dad : misho")
+
+print("my grandma : ala")
+
+print("my grandpa : juba")
+
+
